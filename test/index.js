1b12739abaf068b758f0714cf973331e03c64a98
Johannes Lumpe <johannes@johanneslumpe.de>
2014-08-17 21:48:46 +0300

    Only assign Promise polyfill to window.Promise when it is not defined

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 'use strict';
+/* globals window: true */
 
 // use es6-promise during the tests for browser who
 // do not support promises natively.
-var Promise = require('es6-promise').Promise;
+if (!window.Promise) {
+  window.Promise = require('es6-promise').Promise;
+}
 
 describe('Unit tests', function () {
   require('./unit');
